1d5a4a3709de64b9de9bfe971ec4840544926b33
victor gonzalez <v.gonzalezgomez.vg@gmail.com>
2022-07-09 16:31:58 -0400

    add favicon and alt text for every img

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -5,24 +5,34 @@ button.addEventListener("click", () => {
   let amountStickerOne = document.getElementById("amountsticker1").value;
   let amountStickerTwo = document.getElementById("amountsticker2").value;
   let amountStickerThree = document.getElementById("amountsticker3").value;
-// calculate total of sticker and changing the value of every input to a int number
-  let totalstickers = parseInt(amountStickerOne) + parseInt(amountStickerTwo) + parseInt(amountStickerThree);
+  // calculate total of sticker and changing the value of every input to a int number
+  let totalstickers =
+    parseInt(amountStickerOne) +
+    parseInt(amountStickerTwo) +
+    parseInt(amountStickerThree);
 
-//input validation or return of total sticker 
+  //input validation or return of total sticker
   if (isNaN(totalstickers)) {
-      document.getElementById("totalstickers").innerHTML = "¡¡¡¡Favor de ingresar un numero, dato ingresado no es valido!!!!";
+    document.getElementById("totalstickers").innerHTML =
+      "¡¡¡¡Favor de ingresar un numero, dato ingresado no es valido!!!!";
+  } else if (
+    amountStickerOne < 0 ||
+    amountStickerTwo < 0 ||
+    amountStickerThree < 0
+  ) {
+    document.getElementById("totalstickers").innerHTML =
+      "¡¡¡¡Favor de ingresar un numero positivo, dato ingresado no es valido!!!!";
+  } else if (totalstickers === 1) {
+    document.getElementById(
+      "totalstickers"
+    ).innerHTML = `llevas ${totalstickers} sticker`;
+  } else if (totalstickers <= 10) {
+    document.getElementById(
+      "totalstickers"
+    ).innerHTML = `llevas ${totalstickers} stickers`;
+  } else {
+    document.getElementById(
+      "totalstickers"
+    ).innerHTML = `llevas demasiados stickers`;
   }
-  else if (amountStickerOne < 0 || amountStickerTwo < 0 || amountStickerThree < 0){
-      document.getElementById("totalstickers").innerHTML = "¡¡¡¡Favor de ingresar un numero positivo, dato ingresado no es valido!!!!";
-  }
-  else if  (totalstickers === 1){
-      document.getElementById("totalstickers").innerHTML = `llevas ${totalstickers} sticker`;
-    }
-  else if(totalstickers <= 10){
-      document.getElementById("totalstickers").innerHTML = `llevas ${totalstickers} stickers`;
-    }
-  else {
-      document.getElementById("totalstickers").innerHTML = `llevas demasiados stickers`;
-    }
-
 });
